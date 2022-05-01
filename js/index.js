d53816bfdcf9ff436354679dd594fe8104cc26af
ishimwezachee <zacheeishimwe@gmail.com>
2022-05-01 09:28:47 +0200

    empliment the feedbacks

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let currentdate = new Date(); 
+let  datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + ":"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+
+const currentYear = currentdate.getFullYear()
+document.getElementById("one").innerHTML = `© ${currentYear} | zachee Ishimwe | Kigali - Rwanda`
+document.getElementById("two").innerHTML = `Last Updated: ${datetime}(Central African Time)`
